ac012bd4f26ad767bef7eefd833f60223f406a6a
Kross Ruiz <krossruiz@gmail.com>
2018-07-20 00:42:13 -0700

    Cleaned up movement script to seperate action requests from actions themselves. Also added lateral movement. Slightly buggy rigidbody dynamics right now, possibly because of -x scale box colliders on facing direction change.

--- Assets/A New Start/PlayerMovementManager.cs
+++ Assets/A New Start/PlayerMovementManager.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class PlayerMovementManager : MonoBehaviour {
 
+	public float running_force_magnitude = 113.3f;
+	public float max_speed = 7.7f;
+
+	private Rigidbody rb;
 	private Animator animator;
 	private PlayerAnimationManager pam;
 	public CharacterFacingDirections initial_facing_direction = CharacterFacingDirections.FACING_RIGHT;
 
 	// Use this for initialization
 	void Start () {
+		rb = this.GetComponent<Rigidbody> ();
 		animator = this.GetComponentInChildren<Animator> ();
 		pam = this.GetComponentInChildren<PlayerAnimationManager>();
 
 		switch(initial_facing_direction){
 		case(CharacterFacingDirections.FACING_LEFT):
-			Debug.Log ("FACING LEFT");
 			animator.SetBool (PlayerAnimatorParameters.is_facing_left, true);
 			break;
 		case(CharacterFacingDirections.FACING_RIGHT):
-			Debug.Log ("FACING RIGHT");
 			animator.SetBool (PlayerAnimatorParameters.is_facing_right, true);
 			break;
 		default:
-			Debug.Log ("Where are you facing?!");
 			break;
 		}
 	}
@@ -37,47 +39,67 @@ public class PlayerMovementManager : MonoBehaviour {
 		if (
 			animator.GetCurrentAnimatorStateInfo (PlayerAnimatorStates.default_anim_layer_index).IsName (PlayerAnimatorStates.idle_state_id)
 		) {
-				pam.trigger_jab ();
+			jab ();
 		}
 		if (
 			animator.GetCurrentAnimatorStateInfo (PlayerAnimatorStates.default_anim_layer_index).IsName (PlayerAnimatorStates.jump_state_id) &&
 			animator.GetBool(PlayerAnimatorParameters.enable_jump_punch)){
-			pam.trigger_jab();
+			jab ();
 		}
 		if(
 			animator.GetCurrentAnimatorStateInfo (PlayerAnimatorStates.default_anim_layer_index).IsName (PlayerAnimatorStates.jab_state_id) &&
 			!animator.GetBool (PlayerAnimatorParameters.enable_ground_jab_cooldown)
 		){
-			pam.trigger_jab ();
+			jab ();
 		}
 		if (animator.GetCurrentAnimatorStateInfo (PlayerAnimatorStates.default_anim_layer_index).IsName (PlayerAnimatorStates.jump_punch_state_id) &&
 			!animator.GetBool (PlayerAnimatorParameters.enable_jump_jab_cooldown)) {
-			pam.trigger_jab ();
+			jab ();
 		}
+	}
+
+	private void jab(){
+		pam.trigger_jab ();
 	}
 
 	public void request_death(){
 		if (!animator.GetCurrentAnimatorStateInfo(PlayerAnimatorStates.default_anim_layer_index).IsName (PlayerAnimatorStates.death_state_id)) {
-			pam.trigger_death ();
+			death ();
 		}
+	}
+
+	private void death(){
+		pam.trigger_death ();
 	}
 
 	public void request_revive(){
 		if (animator.GetCurrentAnimatorStateInfo (PlayerAnimatorStates.default_anim_layer_index).IsName (PlayerAnimatorStates.death_state_id)) {
-			pam.trigger_revive ();	
+			revive ();
 		}
+	}
+
+	private void revive(){
+		pam.trigger_revive ();
 	}
 
 	public void request_jump(){
 		if (animator.GetCurrentAnimatorStateInfo (PlayerAnimatorStates.default_anim_layer_index).IsName (PlayerAnimatorStates.idle_state_id)) {
-			pam.trigger_jump ();	
+			jump ();
 		}
+	}
+
+	private void jump(){
+		pam.trigger_jump ();
 	}
 
 	public void request_landing(){
 		if(animator.GetCurrentAnimatorStateInfo (PlayerAnimatorStates.default_anim_layer_index).IsName(PlayerAnimatorStates.jump_state_id)){
-			pam.trigger_landing ();
+			landing ();
 		}
+	}
+
+	private void landing(){
+		pam.trigger_landing ();
 	}
 
 	public void request_run_left(){
@@ -89,9 +111,11 @@ public class PlayerMovementManager : MonoBehaviour {
 		}
 	}
 
-	public void run_left(){
+	private void run_left(){
 		if(animator.GetBool(PlayerAnimatorParameters.is_facing_right))
 			this.transform.localScale = new Vector3 (-this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
+		if (Mathf.Abs (rb.velocity.x) < max_speed)
+			rb.AddForce (running_force_magnitude * Vector3.left);
 		pam.run_left();
 	}
 
@@ -104,9 +128,11 @@ public class PlayerMovementManager : MonoBehaviour {
 		}
 	}
 
-	public void run_right(){
+	private void run_right(){
 		if(animator.GetBool(PlayerAnimatorParameters.is_facing_left))
 			this.transform.localScale = new Vector3 (-this.transform.localScale.x, this.transform.localScale.y, this.transform.localScale.z);
+		if (Mathf.Abs (rb.velocity.x) < max_speed)
+			rb.AddForce (running_force_magnitude * Vector3.right);
 		pam.run_right ();
 	}
 
@@ -114,7 +140,11 @@ public class PlayerMovementManager : MonoBehaviour {
 		if (
 			animator.GetCurrentAnimatorStateInfo (PlayerAnimatorStates.default_anim_layer_index).IsName (PlayerAnimatorStates.idle_state_id) ||
 			animator.GetCurrentAnimatorStateInfo (PlayerAnimatorStates.default_anim_layer_index).IsName (PlayerAnimatorStates.running_state_id)) {
-			pam.stop_running ();	
+			stop_running ();
 		}
+	}
+
+	private void stop_running(){
+		pam.stop_running ();
 	}
 }
