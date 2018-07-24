704b6de3e9fd9b9d9bab6a60110879a90d006da9
Kross Ruiz <krossruiz@gmail.com>
2018-07-24 02:47:43 -0700

    Punch bag has an animation that's triggered. Very simple timing and state transitions, needs work.

--- Assets/A New Start/PunchingBagAnimationManager.cs
+++ Assets/A New Start/PunchingBagAnimationManager.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PunchingBagAnimationManager : MonoBehaviour {
 
+	private Animator animator;
+
 	// Use this for initialization
 	void Start () {
-		
+		animator = this.GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -17,6 +19,7 @@ public class PunchingBagAnimationManager : MonoBehaviour {
 	void OnCollisionEnter(Collision col){
 		if (col.collider.name == "left_hand") {
 			Debug.Log ("You hit me!");
+			animator.SetTrigger ("jabbing");
 		}
 	}
 }
